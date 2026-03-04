db98640a333fd131af38fa55ea4f66adf0887190
Andy Deng <andy@adeng.xyz>
2026-03-05 00:37:56 +0800

    feat: integrate Vercel Analytics and Speed Insights into the application.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import { RootProvider } from "fumadocs-ui/provider/next";
 import "./global.css";
 import { IBM_Plex_Mono } from "next/font/google";
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const ibmPlexMono = IBM_Plex_Mono({
   subsets: ["latin"],
@@ -38,6 +40,8 @@ export default function Layout({ children }: LayoutProps<"/">) {
     <html lang="en" className={ibmPlexMono.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
